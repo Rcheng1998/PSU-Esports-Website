fe72d5a1390aafab639a5ec61d38823ed90fb052
RCheng <rcheng3013@gmail.com>
2019-10-22 16:58:18 -0400

    changed script.js to jquery
    changed script.js
    - changed functionality language from javascript to jquery in order to fix bug in github pages

--- js/script.js
+++ js/script.js
@@ -1,23 +1,6 @@
-var divs = ["Div1", "Div2", "Div3", "Div4", "Div5","Div6", "Div7", "Div8", "Div9", "Div10", "Div11", "Div12", "Div13"];
-var visibleDivId = null;
-function divVisibility(divId) {
-  if(visibleDivId === divId) {
-    visibleDivId = null;
-  } else {
-    visibleDivId = divId;
-  }
-  hideNonVisibleDivs();
-}
-
-function hideNonVisibleDivs() {
-  var i, divId, div;
-  for(i = 0; i < divs.length; i++) {
-    divId = divs[i];
-    div = document.getElementById(divId);
-    if(visibleDivId === divId) {
-      div.style.display = "block";
-    } else {
-      div.style.display = "none";
-    }
-  }
-}
+var $contents = $('.tab-content');
+$contents.slice(1).hide();
+$('.link').click(function() {
+  var $target = $('#' + this.id + 'show').show();
+  $contents.not($target).hide();
+});
